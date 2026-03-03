9de4b942140f76bfd897115c8f208415b5388c70
Mendokoe <byzel38@gmail.com>
2026-03-04 00:30:09 +0700

    raviole: Bringup AviumUi

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -22,9 +22,29 @@ PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Avium Flags
+AVIUM_VERSION_APPEND_TIME_OF_DAY := true
+AVIUM_MAINTAINER := srly8
+
+# Settings
+AVIUM_SETTINGS_SOC_MODEL_NAME := Google Tensor Gen 1
+AVIUM_SETTINGS_DEVICE_CODENAME := oriole
+
+# GMS
+WITH_GMS := true
+
+# Google IME
+TARGET_INCLUDE_GOOGLEIME := true
+TARGET_GOOGLEIME_OVERRIDE_IME := true
+
+# Spoof Props
+AVIUM_FORCE_SET_FAKE_PROP := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="oriole-user 16 BP4A.251205.006 14401865 release-keys" \
     BuildFingerprint=google/oriole/oriole:16/BP4A.251205.006/14401865:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
--- lineage_raven.mk
+++ lineage_raven.mk
@@ -22,9 +22,28 @@ PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
 TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
+# Avium Flags
+AVIUM_VERSION_APPEND_TIME_OF_DAY := true
+AVIUM_MAINTAINER := srly8
+
+# Settings
+AVIUM_SETTINGS_SOC_MODEL_NAME := Google Tensor Gen 1
+AVIUM_SETTINGS_DEVICE_CODENAME := raven
+
+# GMS
+WITH_GMS := true
+
+# Google IME
+TARGET_INCLUDE_GOOGLEIME := true
+TARGET_GOOGLEIME_OVERRIDE_IME := true
+
+# Spoof Props
+AVIUM_FORCE_SET_FAKE_PROP := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="raven-user 16 BP4A.251205.006 14401865 release-keys" \
     BuildFingerprint=google/raven/raven:16/BP4A.251205.006/14401865:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
+
