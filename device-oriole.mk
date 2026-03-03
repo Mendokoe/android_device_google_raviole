4492b2a29d1b1b4678a99ab431da241d50d38e0a
Mendokoe <byzel38@gmail.com>
2026-03-04 01:06:06 +0700

    raviole: add pixel camera inbuilt

--- device-oriole.mk
+++ device-oriole.mk
@@ -14,6 +14,9 @@ TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
 # Inherit from gs101
 include device/google/gs101/device-shipping-common.mk
 
+# Camera
+$(call inherit-product-if-exists, vendor/google/camera/config.mk)
+
 # Overlays
 PRODUCT_PACKAGES += \
     DeviceAsWebcamRaviole \
