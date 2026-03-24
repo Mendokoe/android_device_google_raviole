de8aa9084c38e62f272b1feed1844b8d047da259
Mendokoe <byzel38@gmail.com>
2026-03-24 21:36:18 +0100

    raviole: Bringup @AlphaDroid-Project

---
+++ alpha_raven.mk
@@ -0,0 +1,66 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common stuff
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+
+# Inherit device configuration
+DEVICE_CODENAME := raven
+DEVICE_PATH := device/google/raviole
+VENDOR_PATH := vendor/google/raven
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 6 Pro
+PRODUCT_NAME := alpha_$(DEVICE_CODENAME)
+
+# append time of day to zip
+ALPHA_VERSION_APPEND_TIME_OF_DAY := false
+
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 1
+
+# Debugging
+TARGET_INCLUDE_MATLOG := true
+
+# Extras
+TARGET_INCLUDE_SIMPLE_TUNE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+ifeq ($(TARGET_BUILD_PACKAGE),3)
+  # (valid only for GAPPS builds)
+  TARGET_INCLUDE_GOOGLE_COMMS := true
+  TARGET_INCLUDE_PIXEL_LAUNCHER := true
+  TARGET_SUPPORTS_CALL_RECORDING := true
+  TARGET_INCLUDE_STOCK_ARCORE := true
+  TARGET_INCLUDE_LIVE_WALLPAPERS := true
+  TARGET_SUPPORTS_GOOGLE_RECORDER := false
+endif
+
+# Maintainer
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := srly8
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 3120
+TARGET_SCREEN_WIDTH := 1440
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="raven-user 16 BP4A.251205.006 14401865 release-keys" \
+    BuildFingerprint=google/raven/raven:16/BP4A.251205.006/14401865:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
